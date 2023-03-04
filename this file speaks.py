4ecf65936f866101568621cc3ebb490873249b2f
X Æ M <79490067+XA-12@users.noreply.github.com>
2023-03-04 09:27:55 +0100

    Update this file speaks.py

--- this file speaks.py
+++ this file speaks.py
@@ -8,3 +8,11 @@ for i in range(2):
     x.say(txt)
     x.runAndWait()
 
+''' 
+the engine speaks whatever text is passed to it using the say() method. 
+
+using the runAndWait() method, it blocks during the event loop and returns when the commands queue is cleared
+can also use the .setproperty() to set speech rate and volume
+ 
+
+ '''
